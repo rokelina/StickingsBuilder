23fbd495f68a7ed3aa7feb41e3c41e31ada82285
Rosina Scampino <rosinascampino@gmail.com>
2023-06-05 09:32:49 -0400

    add min values to top navbar

--- src/components/TopNavBar/TopNavBar.tsx
+++ src/components/TopNavBar/TopNavBar.tsx
@@ -11,6 +11,7 @@ function TopNavBar() {
         icon={<MetronomeIcon></MetronomeIcon>}
         wrapperName="metronome"
         inputName="met-input"
+        minValue={20}
         spanText="BPM"
         defaultCount={90}
       ></NumberInput>
@@ -18,6 +19,7 @@ function TopNavBar() {
         icon={<RepeatIcon></RepeatIcon>}
         wrapperName="repeat"
         inputName="rep-input"
+        minValue={1}
         spanText="Repeats"
         defaultCount={4}
       ></NumberInput>
