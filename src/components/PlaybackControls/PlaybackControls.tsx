ab4f9c9bababe6bc188e73b2b109ad2c94ae3579
Rosina Scampino <rosinascampino@gmail.com>
2023-07-10 15:22:55 -0400

    add sound controls

--- src/components/PlaybackControls/PlaybackControls.tsx
+++ src/components/PlaybackControls/PlaybackControls.tsx
@@ -3,11 +3,27 @@ import './PlaybackControls.css';
 
 function PlaybackControls() {
   return (
-    <div className="controls">
-      <Button idName="Play" children="▶ Play" />
-      <Button idName="Pause" children="⏸ Pause" />
-      <Button idName="Stop" children="⏹ Stop" />
-    </div>
+    <>
+      <div className="playback-controls">
+        <Button idName="Play" children="▶ Play" />
+        <Button idName="Pause" children="⏸ Pause" />
+        <Button idName="Stop" children="⏹ Stop" />
+      </div>
+      <div className="sound-controls">
+        <label htmlFor="metronome-sound">
+          Metronome
+          <input type="checkbox" name="metronome-sound" id="metronome-sound" />
+        </label>
+        <label htmlFor="snare-sound">
+          Snare
+          <input type="checkbox" name="snare-sound" id="snare-sound" />
+        </label>
+        <label htmlFor="volume">
+          Volume
+          <input type="range" name="volume" id="volume" />
+        </label>
+      </div>
+    </>
   );
 }
 export default PlaybackControls;
