3f4a9154650c9af1490b8f6535797752eb0be000
Rosina Scampino <rosinascampino@gmail.com>
2023-08-04 12:20:16 -0400

    add drawNotes to props

--- src/components/RandomMode/RandomStaff.tsx
+++ src/components/RandomMode/RandomStaff.tsx
@@ -1,10 +1,21 @@
 import { useEffect, useRef } from 'react';
-import { RenderContext, Stave, Tuplet, Beam, Formatter } from 'vexflow';
+import {
+  RenderContext,
+  StaveNote,
+  Stave,
+  Tuplet,
+  Beam,
+  Formatter,
+} from 'vexflow';
 import drawStaff from '../../lib/utils/staffUtils/drawStaff';
 import drawRandomNotes from '../../lib/utils/randomModeUtils/drawRandomNotes';
 import '../Staff/Staff.css';
 interface Props {
   generatedStickings: { [key: string]: string };
+  drawNotes?: (
+    stickings: { [key: string]: string },
+    beatName: string
+  ) => StaveNote[];
 }
 
 function RandomStaff({ generatedStickings }: Props) {
@@ -19,14 +30,15 @@ function RandomStaff({ generatedStickings }: Props) {
     const notes3 = drawRandomNotes(generatedStickings, 'beat-3');
     const notes4 = drawRandomNotes(generatedStickings, 'beat-4');
     const allNotes = [...notes1, ...notes2, ...notes3, ...notes4];
-
     const allBeats = [notes1, notes2, notes3, notes4];
+
     const beams = [
       new Beam(notes1),
       new Beam(notes2),
       new Beam(notes3),
       new Beam(notes4),
     ];
+
     const tuplets: Tuplet[] = allBeats
       .filter((notesArray) => notesArray.length === 3 || notesArray.length > 4)
       .map((notesArray) => new Tuplet(notesArray, { ratioed: false }));
