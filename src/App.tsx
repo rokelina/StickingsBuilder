39a92fcda4fd76343ae0d7fb0bd2699b23304dc9
Rosina Scampino <rosinascampino@gmail.com>
2023-08-14 18:26:15 -0400

    add login btns

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect, CSSProperties } from 'react';
+// import { useLoadingSpinner } from './hooks/useLoadingSpinner';
 import MainWrapper from './components/MainWrapper/MainWrapper';
 import SideNavBar from './components/SideNavBar/SideNavBar';
+// import BarLoader from 'react-spinners/BarLoader';
 
 function App() {
   const [showMenu, setShowMenu] = useState('eighth-notes');
@@ -11,8 +13,14 @@ function App() {
 
   return (
     <>
-      <SideNavBar onNavClick={handleNavClick} />
-      <MainWrapper displayMenu={showMenu} />
+      {/* {isLoading ? (
+        <BarLoader />
+      ) : ( */}
+      <>
+        <SideNavBar onNavClick={handleNavClick} />
+        <MainWrapper displayMenu={showMenu} />
+      </>
+      {/* )} */}
     </>
   );
 }
--- src/components/TopNavBar/TopNavBar.tsx
+++ src/components/TopNavBar/TopNavBar.tsx
@@ -1,9 +1,16 @@
+import Button from '../Button/Button';
 import './TopNavBar.css';
 
 function TopNavBar() {
+  // add log in buttons
+
   return (
     <nav className="top-nav">
       <h1>Stickings Builder</h1>
+      <div className="login-signup">
+        <Button idName="log-in-btn">Log In</Button>
+        <Button idName="sign-up-btn">Sign Up</Button>
+      </div>
     </nav>
   );
 }
