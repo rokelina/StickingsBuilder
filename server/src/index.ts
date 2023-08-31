d7ba7f4fad677e1185a4b057e6709c0ce91131e4
Rosina Scampino <rosinascampino@gmail.com>
2023-08-31 10:25:30 -0400

    add ts compiler to server

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+console.log('Hello world');
+
+// const express = require('express');
+
+// const port = 5000;
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// });
+
+// app.listen(port, () => console.log(`Listening in port ${port}`));
