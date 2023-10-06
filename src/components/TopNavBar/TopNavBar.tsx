be56c172887d891d7ae1fd8e016ae6c3baaafd4f
Rosina Scampino <rosinascampino@gmail.com>
2023-10-06 09:45:08 -0400

    nav bar media queries

--- src/components/TopNavBar/TopNavBar.tsx
+++ src/components/TopNavBar/TopNavBar.tsx
@@ -1,16 +1,18 @@
-// import Button from '../Button/Button';
+import Button from '../Button/Button';
 import Snare from '../../assets/icons/Snare';
 import './TopNavBar.css';
 
 function TopNavBar() {
   return (
     <nav className="top-nav">
-      <Snare />
-      <h1>STICKINGS BUILDER</h1>
-      {/* <div className="login-signup">
+      <div className="title-logo">
+        <Snare />
+        <h1>STICKINGS BUILDER</h1>
+      </div>
+      <div className="login-signup">
         <Button idName="log-in-btn">LOG IN</Button>
         <Button idName="sign-up-btn">SIGN UP</Button>
-      </div> */}
+      </div>
     </nav>
   );
 }
