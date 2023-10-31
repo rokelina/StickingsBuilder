e36efad425b1c225ecab125bfaabdacf68085e4c
Rosina Scampino <rosinascampino@gmail.com>
2023-10-30 21:29:35 -0400

    add manifest to config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,43 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    svgr(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Stickings Builder App',
+        short_name: 'Stickings Builder',
+        description:
+          'An application for drummers and percussionists aimed to help them improve their technical dexterity by practicing different sticking combinations. Inspired by the book Stick Control written by George L. Stone',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'public/pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'public/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'public/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'public/maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
