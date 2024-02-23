107aefd2bbdca00a8ba21776ad9889f0ec2a2829
Rosina Scampino <rosinascampino@gmail.com>
2024-02-23 18:49:08 -0500

    add log in and about buttons

--- src/components/SideNavBar/SideNavBar.tsx
+++ src/components/SideNavBar/SideNavBar.tsx
@@ -2,6 +2,7 @@ import MenuIcon from '../Icons/MenuIcon';
 import EightNotesIcon from '../Icons/EightNotesIcon';
 import RandomIcon from '../Icons/RandomIcon';
 import TripletsIcon from '../Icons/TripletsIcon';
+import SaveIcon from '../Icons/SaveIcon';
 import ListItem from './ListItem/ListItem';
 import './SideNavBar.css';
 
@@ -37,6 +38,35 @@ function SideNavBar({ onNavClick }: Props) {
           spanText="Random Stickings"
           onNavClick={onNavClick}
         />
+        <ListItem
+          ariaLabel="saved stickings menu"
+          idName="saved-stickings"
+          icon={<SaveIcon />}
+          spanText="Saved Stickings"
+          onNavClick={onNavClick}
+        />
+      </ul>
+      <ul className="navbar-nav" style={{ flex: 0 }}>
+        <li className="nav-item" id="log-in">
+          <button
+            className="nav-link"
+            aria-label="log in button"
+            onClick={() => console.log('log in clicked')}
+          >
+            {<SaveIcon />}
+            <span className="icon-text">Log In</span>
+          </button>
+        </li>
+        <li className="nav-item" id="about">
+          <button
+            className="nav-link"
+            aria-label="about this application"
+            onClick={() => console.log('about clicked')}
+          >
+            {<RandomIcon />}
+            <span className="icon-text">About</span>
+          </button>
+        </li>
       </ul>
     </nav>
   );
