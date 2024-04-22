170880b362316814cd25f7f6509f35423975ac1e
Rosina Scampino <rosinascampino@gmail.com>
2024-04-22 17:00:33 -0400

    add auth context and provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AuthProvider from './context/AuthProvider';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <>
     <React.StrictMode>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </React.StrictMode>
   </>
 );
