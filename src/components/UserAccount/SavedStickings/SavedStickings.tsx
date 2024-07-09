77c5b9b73e72ac769a425a9bdd3bfeb1c9152da4
Rosina Scampino <rosinascampino@gmail.com>
2024-07-09 11:11:31 -0500

    add my saved stickings component

---
+++ src/components/UserAccount/SavedStickings/SavedStickings.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import Button from '../../Button/Button';
+import './SavedStickings.css';
+
+const SavedStickings = () => {
+  const [showStickings, setShowStickings] = useState(true);
+  const handleOnClick = () => {
+    setShowStickings(!showStickings);
+  };
+
+  return (
+    <div className="saved-stickings-card">
+      <Button
+        idName={'show-saved-stickins'}
+        children={'View my saved stickings'}
+        onBtnClick={handleOnClick}
+      />
+      {showStickings && (
+        <div>
+          <p>Hello</p>
+        </div>
+      )}
+    </div>
+  );
+};
+export default SavedStickings;
--- src/components/UserAccount/UserAccount.tsx
+++ src/components/UserAccount/UserAccount.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from '../../context/authContext/useAuth';
 import { Samples } from '../../hooks/useSamples';
 import Button from '../Button/Button';
+import SavedStickings from './SavedStickings/SavedStickings';
 
 type UserAccountProps = {
   samples: Samples;
@@ -10,14 +11,17 @@ const UserAccount = ({ samples }: UserAccountProps) => {
   const { authUser, signOut } = useAuth();
 
   return (
-    <div className="account-menu">
-      <h2 className="welcome-message">Welcome {authUser?.name}!</h2>
-      <Button
-        idName="sign-out-btn"
-        children="Sign Out"
-        onBtnClick={signOut}
-      ></Button>
-    </div>
+    <>
+      <div className="account-menu">
+        <h2 className="welcome-message">Welcome {authUser?.name}!</h2>
+        <Button
+          idName="sign-out-btn"
+          children="Sign Out"
+          onBtnClick={signOut}
+        ></Button>
+      </div>
+      <SavedStickings />
+    </>
   );
 };
 export default UserAccount;
