b8653b44ff2656970f3324468518b64d20bd596f
Rosina Scampino <rosinascampino@gmail.com>
2024-07-29 15:54:24 -0500

    fix env variables

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.STICKINGS_BUILDER_API_KEY,
-  authDomain: process.env.STICKINGS_BUILDER_AUTH_DOMAIN,
-  projectId: process.env.STICKINGS_BUILDER_PROJECT_ID,
-  storageBucket: process.env.STICKINGS_BUILDER_STORAGE_BUCKET,
-  messagingSenderId: process.env.STICKINGS_BUILDER_MESSAGING_SENDER_ID,
-  appId: process.env.STICKINGS_BUILDER_APP_ID,
-  measurementId: process.env.STICKINGS_BUILDER_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_STICKINGS_BUILDER_API_KEY,
+  authDomain: import.meta.env.VITE_STICKINGS_BUILDER_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_STICKINGS_BUILDER_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STICKINGS_BUILDER_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_STICKINGS_BUILDER_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_STICKINGS_BUILDER_APP_ID,
+  measurementId: import.meta.env.VITE_STICKINGS_BUILDER_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
